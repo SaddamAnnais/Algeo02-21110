c9b11760b353089676b9e5ac73deca6bacbc8f4c
SaddamAnnais <saddamannaiss@gmail.com>
2022-11-11 20:09:43 +0700

    Done, berhasil predict face

---
+++ src/predictFace.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+from utils import *
+
+nama = np.load('src/nama.npy')
+mean = np.load('src/mean.npy')
+E = np.load('src/E.npy')
+Y = np.load('src/Y.npy')
+D = np.load('src/D.npy')
+dataset = np.load('src/dataset.npy')
+
+# Predict new face
+def predict(dirGambar, mean, E, Y, D, dataset, nama): 
+    D = D[0]
+    newFace = read_image(dirGambar)
+    cv2.imshow("test", newFace.reshape((256,256)))
+    cv2.waitKey(0)
+
+    newFace = (newFace - mean).reshape(256*256,1)
+    newWeight = E.T @ newFace
+
+    # Mencari yg paling mirip menggunakan eucledian distance
+    weightSelisih = newWeight - Y[:,0].reshape(D,1)
+    selisih = eucledian(weightSelisih)
+    idxHasil = 0
+
+    for i in range(1, Y.shape[1]):
+        weightSelisih = newWeight - Y[:,i].reshape(D,1)
+        tempSelisih = eucledian(weightSelisih)
+        if selisih > tempSelisih :
+            idxHasil = i
+            selisih = tempSelisih
+            print(selisih)
+
+    # print(idxHasil)
+    matHasil = dataset[idxHasil]
+    print(nama[idxHasil])
+    print(idxHasil)
+    cv2.imshow("hasil", matHasil.reshape(256,256))
+    cv2.waitKey(0)  
+
+predict("gal gadot90_1846.jpg", mean, E, Y, D, dataset, nama)
---
+++ src/utils.py
@@ -0,0 +1,61 @@
+import numpy as np
+import copy
+import cv2
+import mediapipe as mp
+
+
+
+def eucledian(array):
+    sum = np.sum(np.square(array))
+    return np.sqrt(sum)
+
+def normalize(array):
+    normalizedArray = (array-np.min(array))/(np.max(array)-np.min(array))
+    return normalizedArray
+
+def crop_image(img, size):
+    # input gambar berwarna, output gambar berwarna
+    # mengcropped image sehingga hanya tampil bagian mukanya saja
+    img2 = copy.deepcopy(img)
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5, flags=cv2.CASCADE_SCALE_IMAGE)
+    if (isinstance(faces, np.ndarray)):     # jika wajah terdeteksi, maka dicrop
+        for (x, y, w, h) in faces:
+            img2 = img[y:y+h,x:x+w]
+    resized = cv2.resize(img2, (size, size), interpolation=cv2.INTER_AREA)
+    return resized
+
+def crop_square(img, size, interpolation=cv2.INTER_AREA):
+    h, w = img.shape[:2]
+    min_size = np.amin([h,w])
+
+    # Centralize and crop
+    crop_img = img[int(h/2-min_size/2):int(h/2+min_size/2), int(w/2-min_size/2):int(w/2+min_size/2)]
+    resized = cv2.resize(crop_img, (size, size), interpolation=interpolation)
+
+    return resized
+
+def remove_bg(img):
+    # Menghilangkan background dari gambar sehingga training jadi lebih akurat
+    # Input gambar berwarna ukuran (256,256,3)
+    # Output gambar grayscale ukuran (256,256)
+    mp_selfie_segmentation = mp.solutions.selfie_segmentation
+    selfie_segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
+    results = selfie_segmentation.process(img)
+
+    mask = results.segmentation_mask
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    conditionMask = mask > 0.5
+    bg_image = np.zeros(shape=(img.shape[0],img.shape[1]))
+    output_image = np.where(conditionMask,img,bg_image).astype(np.uint8)
+    if (output_image == bg_image).all():
+        return img
+    else :
+        return output_image
+
+def read_image(img_dir):
+    rawImage = cv2.imread(img_dir)                 # Read image
+    noBgImage = remove_bg(rawImage)                # Remove background from image
+    processedImage = crop_image(noBgImage, 256)  # Crop to face and resize it to (256, 256)
+    # processedImage = crop_square(noBgImage, 256)   # Crop image and resize it to (256, 256)
+    return processedImage.reshape(1,256*256)                # Matrix with shape (1, 256x256)
